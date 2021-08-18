c745d3ca6d093fc9e9ac004428de8d9521a2ba38
willyngash <willynganga024@gmail.com>
2021-08-18 16:15:42 +0300

    🥳 Create hilt android app

---
+++ app/src/main/java/ke/co/willynganga/modernhorticulture/ModernHorticultureApp.kt
@@ -0,0 +1,7 @@
+package ke.co.willynganga.modernhorticulture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ModernHorticultureApp : Application()
