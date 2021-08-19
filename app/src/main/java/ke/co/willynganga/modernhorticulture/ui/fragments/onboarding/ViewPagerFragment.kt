442ae1d8cedfa1a12ece6ad336b697ea0a4587de
willyngash <willynganga024@gmail.com>
2021-08-19 20:45:20 +0300

    🔥 code clean up

--- app/src/main/java/ke/co/willynganga/modernhorticulture/ui/fragments/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/ke/co/willynganga/modernhorticulture/ui/fragments/onboarding/ViewPagerFragment.kt
@@ -1,4 +1,4 @@
-package ke.co.willynganga.modernhorticulture.onboarding
+package ke.co.willynganga.modernhorticulture.ui.fragments.onboarding
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,9 +7,9 @@ import android.viewbinding.library.fragment.viewBinding
 import ke.co.willynganga.modernhorticulture.R
 import ke.co.willynganga.modernhorticulture.adapter.ViewPagerAdapter
 import ke.co.willynganga.modernhorticulture.databinding.FragmentViewPagerBinding
-import ke.co.willynganga.modernhorticulture.onboarding.screens.FirstScreen
-import ke.co.willynganga.modernhorticulture.onboarding.screens.SecondScreen
-import ke.co.willynganga.modernhorticulture.onboarding.screens.ThirdScreen
+import ke.co.willynganga.modernhorticulture.ui.fragments.onboarding.screens.FirstScreen
+import ke.co.willynganga.modernhorticulture.ui.fragments.onboarding.screens.SecondScreen
+import ke.co.willynganga.modernhorticulture.ui.fragments.onboarding.screens.ThirdScreen
 
 class ViewPagerFragment : Fragment(R.layout.fragment_view_pager) {
 
