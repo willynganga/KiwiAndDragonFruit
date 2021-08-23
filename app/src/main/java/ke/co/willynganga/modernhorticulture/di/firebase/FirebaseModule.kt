603c7b57f25609ee96498e77342c2df875ae2762
willyngash <willynganga024@gmail.com>
2021-08-23 19:23:33 +0300

    🔥 create Firebase hilt module

---
+++ app/src/main/java/ke/co/willynganga/modernhorticulture/di/firebase/FirebaseModule.kt
@@ -0,0 +1,37 @@
+package ke.co.willynganga.modernhorticulture.di.firebase
+
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ke.co.willynganga.modernhorticulture.repository.FirebaseAuthenticatorRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuthenticator(
+        auth: FirebaseAuth
+    ): Authenticator {
+        return FirebaseAuthenticatorRepository(auth)
+    }
+
+}
